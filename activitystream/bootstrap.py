76f658a472654d054fd1a7254f9f4cb5a9884044
Lorenzo Giacosa <lorenzo.giacosa@axant.it>
2016-09-06 08:09:02 +0200

    Removed model bootstrap.

--- activitystream/bootstrap.py
+++ activitystream/bootstrap.py
@@ -2,15 +2,6 @@
 """Setup the activitystream application"""
 from __future__ import print_function
 
-from activitystream import model
-from tgext.pluggable import app_model
 
 def bootstrap(command, conf, vars):
     print('Bootstrapping activitystream...')
-
-    s1 = model.Sample()
-    s1.name = 'Test Sample'
-    s1.user = model.DBSession.query(app_model.User).first()
-
-    model.DBSession.add(s1)
-    model.DBSession.flush()
